769263dfc0e4ccfd6995df10a7cdd72055ce642d
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2025-03-04 23:43:24 +0100

    simple start: read chain manager and print block tip
    can be executed via
    $ uv run main.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+#!/usr/bin/env python3
+from pathlib import Path
+import sys
+
+import pbk
+
+
 def main():
-    print("Hello from ibd-booster-hints-gen!")
+    #log = pbk.LoggingConnection()
+    datadir = Path.home() / ".bitcoin"
+    print("Loading chain manager... ", end='', flush=True)
+    chainman = pbk.load_chainman(datadir, pbk.ChainType.MAINNET)
+    print("done.")
+
+    # TODO: read blocks here
+    tip = chainman.get_block_index_from_tip()
+    print(f"Current block tip: {tip.block_hash.hex} at height {tip.height}")
 
 
 if __name__ == "__main__":
